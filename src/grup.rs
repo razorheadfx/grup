9991cb1550016d8d70cf452e71775c76626061b4
Niklas Claesson <nicke.claesson@gmail.com>
2019-09-23 22:09:18 +0200

    Switch to hyper and async/await

--- src/grup.rs
+++ src/grup.rs
@@ -1,26 +1,20 @@
 #[macro_use]
 extern crate log;
-extern crate env_logger;
 
-// md parser + formatter
-extern crate comrak;
-// simple http server
-extern crate simple_server;
-
-// cmdline parsing
-extern crate structopt;
-
-use comrak::ComrakOptions;
-use simple_server::Server;
-use std::fs::File;
-use std::io::Read;
+use std::io;
 use std::net::IpAddr;
 use std::path::PathBuf;
-use std::process;
+use std::sync::Arc;
+
+use comrak::ComrakOptions;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server, StatusCode};
 use structopt::StructOpt;
+use tokio::prelude::*;
+use tokio_fs::File;
 
 #[derive(Debug, StructOpt)]
-/// grup - a offline github markdown previewer
+/// grup - an offline github markdown previewer
 struct Cfg {
     #[structopt(name = "markdown_file", parse(from_os_str))]
     /// The markdown file to be served
@@ -39,85 +33,118 @@ struct Cfg {
     host: IpAddr,
 }
 
+type CfgPtr = Arc<Cfg>;
+
 const DEFAULT_CSS: &[u8] = include_bytes!("../resource/github-markdown.css");
 
-fn main() {
-    env_logger::Builder::from_default_env().init();
-    let cfg = Cfg::from_args();
-    let file = cfg.md_file;
+fn not_found() -> Result<Response<Body>, hyper::Error> {
+    let mut response = Response::builder();
+    response.status(StatusCode::NOT_FOUND);
+    Ok(response
+        .body(Body::from(""))
+        .expect("invalid response builder"))
+}
 
-    // these were parsed and checked by structopt but now we need to turn them back to strings
-    let (host, port) = (format!("{}", cfg.host), format!("{}", cfg.port));
+// TODO: setup notify hook on file
+// - calls of inotify (debounced is fine; take Write Events; reparse and re-render)
+
+async fn md_file(cfg: CfgPtr) -> Result<Response<Body>, hyper::Error> {
+    let mut response = Response::builder();
+    response.header("Content-type", "text/html");
+
+    let content = if let Ok(mut file) = File::open(&cfg.md_file).await {
+        let mut buf = String::new();
+        if file.read_to_string(&mut buf).await.is_ok() {
+            let mut options = ComrakOptions::default();
+            options.hardbreaks = true;
+            comrak::markdown_to_html(&buf, &options)
+        } else {
+            return not_found();
+        }
+    } else {
+        return not_found();
+    };
+    let title = String::from(
+        cfg.md_file
+            .to_str()
+            .unwrap_or(&format!("{:?}", cfg.md_file)),
+    );
+
+    // push it all into a container
+    let document = format!(
+        r#"<!DOCTYPE html>
+         <html>
+            <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+                <style>
+                    body {{
+                    box-sizing: border-box;
+                    min-width: 200px;
+                    max-width: 980px;
+                    margin: 0 auto;
+                    padding: 45px;
+                    }}
+                </style>
+                <link rel="stylesheet" href="style.css">
+                <title>{title}</title>
+            </head>
+            <body>
+            <article class="markdown-body">
+            {content}
+            </article>
+            </body>
+        </html>"#,
+        title = title,
+        content = content,
+    );
+    Ok(response
+        .body(Body::from(document))
+        .expect("invalid response builder"))
+}
+
+async fn css() -> Result<Response<Body>, hyper::Error> {
+    let mut response = Response::builder();
+    response.header("Content-type", "text/css");
+    Ok(response
+        .body(Body::from(DEFAULT_CSS))
+        .expect("invalid response builder"))
+}
+
+async fn router(cfg: CfgPtr, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    match req.uri().path() {
+        "/" => md_file(cfg).await,
+        "/style.css" => css().await,
+        _ => not_found(),
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    env_logger::Builder::from_default_env().init();
+    let cfg = Arc::new(Cfg::from_args());
+    let file = &cfg.md_file;
 
     if !file.exists() {
-        eprintln!("Error: {:#?} does not exist!", file);
-        process::exit(-1);
+        return Err(
+            io::Error::new(io::ErrorKind::Other, format!("No such file: {:?}", file)).into(),
+        );
     }
 
     if !file.is_file() {
-        eprintln!("Error: {:#?} is not a file!", file);
-        process::exit(-1);
+        return Err(
+            io::Error::new(io::ErrorKind::Other, format!("No such file: {:?}", file)).into(),
+        );
     }
 
-    // TODO: setup notify hook on file
-    // - calls of inotify (debounced is fine; take Write Events; reparse and re-render)
-
-    let mut server = Server::new(move |request, mut response| {
-        info!("Request received. {} {}", request.method(), request.uri());
-
-        // if they want the stylesheet serve it
-        // else give them the formatted MD file
-        if request.uri().path() == "/style.css" {
-            return Ok(response.body(DEFAULT_CSS.to_vec())?);
-        }
-
-        let parsed_and_formatted = File::open(&file)
-            .and_then(|mut f| {
-                let mut s = String::new();
-                f.read_to_string(&mut s).map(|_| s)
-            })
-            .and_then(|md| {
-                let mut options = ComrakOptions::default();
-                options.hardbreaks = true;
-                Ok(comrak::markdown_to_html(&md, &options))
-            })
-            .unwrap_or_else(|e| format!("Grup encountered an error: <br> {:#?}", e));
-
-        let title = String::from(file.to_str().unwrap_or(&format!("{:?}", file)));
-
-        // push it all into a container
-        let doc = format!(
-            r#"<!DOCTYPE html>
-             <html>
-                <head>
-                    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-                    <style>
-                        body {{
-                        box-sizing: border-box;
-                        min-width: 200px;
-                        max-width: 980px;
-                        margin: 0 auto;
-                        padding: 45px;
-                        }}
-                    </style>
-                    <link rel="stylesheet" href="style.css">
-                    <title>{}</title>
-                </head>
-                <body>
-                <article class="markdown-body">
-                {}
-                <article class="markdown-body">
-                </body>
-            </html>"#,
-            title, parsed_and_formatted
-        );
-
-        Ok(response.body(doc.into_bytes())?)
+    let service = make_service_fn(|_| {
+        let cfg = Arc::clone(&cfg);
+        async { Ok::<_, hyper::Error>(service_fn(move |req| router(Arc::clone(&cfg), req))) }
     });
 
-    server.set_static_directory(".");
-
-    println!("Server running at http://{}:{}", host, port);
+    let addr = std::net::SocketAddr::new(cfg.host, cfg.port);
+    let server = Server::bind(&addr).serve(service);
+    println!("Server running at http://{}", addr);
     println!("Press Ctrl-C to exit");
-    server.listen(&host, &port);
+    server.await?;
+    Ok(())
 }
